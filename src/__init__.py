0df43c23ce6a83ea41e23d3d1ac5ea11e92e1097
tez4 <joel.twann@bluewin.ch>
2023-01-20 10:17:40 +0100

    Added new train-test split to moving window

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
-from src.main import read_nz_file, read_jg_file, update_meta_data, split_df, aggregate_files, add_moving_window
+from src.main import (
+    read_nz_file, read_jg_file, update_meta_data, split_df, aggregate_files, add_moving_window, add_moving_window_2)
